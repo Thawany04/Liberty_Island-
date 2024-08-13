d92d8f25f36db0578d2e13ef1d82f2d8b0722c67
adriano064 <141060414+adriano064@users.noreply.github.com>
2024-08-13 11:47:11 -0300

    fases

--- Liberty Island/Assets/Script/GamerControler.cs
+++ Liberty Island/Assets/Script/GamerControler.cs
@@ -1,36 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Gamer_Controler : MonoBehaviour
 {
     public Text vidatext;
     public static Gamer_Controler instace;
 
-    public int score;
+    public int score, quantidade;
     public Text scoreText;
+    public TextMeshProUGUI scoreTextPro;
     
     
     public void Updatescore(int value)
     {
         score += value;
-        scoreText.text = score.ToString();
+        scoreText.text = $"{score} / {quantidade }";
     }
     void Start()
     {
         instace = this;
-    }
-
-   
-    void Update()
-    {
-        
+        quantidade = GameObject.FindGameObjectsWithTag("coletavel").Length;
+        Updatescore(0);
     }
 
     public void UpdateLives(int value)
     {
         vidatext.text = "x " + value.ToString();
+    }
+    private void Update()
+    {
+        if (score>= 3)
+        {
+            SceneManager.LoadScene(1);
+        }
     }
 }
 
