35f37bc74d229084359154d4ea46f8ab3dccffd7
adriano064 <141060414+adriano064@users.noreply.github.com>
2024-11-29 16:32:52 -0300

    99

---
+++ Liberty Island/Assets/Script/po.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class po : MonoBehaviour
+{
+    [SerializeField] private string nomeDaProximaFase; // Nome da próxima fase (definido no Inspector ou no código)
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Verifica se o objeto que colidiu tem a tag "Player"
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("Jogador colidiu, carregando próxima fase: " + nomeDaProximaFase);
+            SceneManager.LoadScene(nomeDaProximaFase); // Carrega a cena especificada
+        }
+    }
+}
